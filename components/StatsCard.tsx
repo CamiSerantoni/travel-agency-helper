89233a50987492ee20bc3e40c3796b03d09091de
CamiSerantoni <camila.serantonir@gmail.com>
2026-03-23 15:00:15 -0300

    starting the cards component

---
+++ components/StatsCard.tsx
@@ -0,0 +1,26 @@
+import { calculateTrendPercentage } from 'lib/utils'
+import React from 'react'
+
+const StatsCard = ({
+    headerTitle, total, lastMonthCount, currentMonthCount
+}: StatsCard) => {
+
+
+const {trend, percentage} =calculateTrendPercentage(currentMonthCount, lastMonthCount)
+const isDecrement = trend === 'decrement'
+
+
+
+
+
+  return (
+
+    <article className='stats-card'>
+        <h3 className='text-base font-medium'> {headerTitle}</h3>
+
+    </article>
+
+  )
+}
+
+export default StatsCard
